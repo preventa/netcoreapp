3c930ae4be562722b8efc7b8b033d29903cee867
Fernando Pereiro <f.pereiro@ibermatica.com>
2017-06-21 13:02:27 +0200

    Creación del proyecto

---
+++ CoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Esta es una app de .NET Core y Docker";
+            ViewData["SistemaOperativo"] = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            ViewData["IdContenedor"] = System.Environment.MachineName;
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
